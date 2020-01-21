a7ed43d29b3777ef070d5007f9d3857af2d64364
fhnribeiro <fhnribeiro@hotmail.com>
2020-01-20 21:30:15 -0300

    End session 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -23,6 +23,8 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values
+
+		[AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
